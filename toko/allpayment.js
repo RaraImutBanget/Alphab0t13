f65223629005f601413de53b4a3d505e292e665e
RaraImutBanget <95358465+RaraImutBanget@users.noreply.github.com>
2022-01-05 17:09:45 +0700

    Update and rename shop/allpayment.js to toko/allpayment.js

---
+++ toko/allpayment.js
@@ -0,0 +1,14 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY RARA
+	
+*💰 E-MONEY*
+
+     ¤ 0812 7165 5147
+
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
